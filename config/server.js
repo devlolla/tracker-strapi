a1b6f62d2a7ce6c1309832387c28426d9c8664fc
devlolla <devlolla@outlook.com>
2021-10-16 05:56:32 -0300

    :ghost:

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", process.env.PORT),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
